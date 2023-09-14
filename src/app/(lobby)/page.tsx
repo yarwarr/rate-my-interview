e99c38e1d43b30c4ab996db453ebbf078c085783
Mubashar <maw8051@rit.edu>
2023-09-13 23:04:22 -0400

    Improved the navbar

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -16,7 +16,7 @@ export default async function IndexPage() {
         className="mx-auto flex w-full max-w-[64rem] flex-col items-center justify-center gap-4 pb-8 pt-6 text-center md:pb-12 md:pt-10 lg:py-32"
       >
         <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:text-6xl lg:leading-[1.1]">
-          A Website
+          A Website A Website where you can rate your interviews
         </h1>
         <Balance className="max-w-[46rem] text-lg text-muted-foreground sm:text-xl">
           Enter the company name
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,44 +3,53 @@ import { Icons } from "./Icons";
 import { buttonVariants } from "./ui/Button";
 import { getAuthSession } from "../lib/auth/index";
 import UserAccountNav from "./UserAccountNav";
-import { ModeToggle } from "./ui/darkmode";
 import { Combobox } from "./combobox";
+import { ThemeToggle } from "./layouts/theme-toggle";
 const Navbar = async () => {
-    const session = await getAuthSession()
-    return (
-        <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
-            <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
-                {/* logo */}
-                <Link href="/" className="flex gap-2 items-center">
-                    <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6" />
-                    <p className="hidden text-zinc-700 text-sm font-medium md:block">
-                        Rate
-                    </p>
-                </Link>
-                <Link href="/company" className="flex gap-2 items-center">
-                    <p className="hidden text-zinc-700 text-sm font-medium md:block">
-                        Companies
-                    </p>
-                </Link>
-                <div className="flex gap-2 items-center">
-                    <Combobox />
-                </div>
-                
-
-                {/* search bar */}
-
-                {session?.user ? (
-                    <UserAccountNav user={session.user} />
-                ) : (
-                    <Link href='/sign-in' className={buttonVariants({
-                        size: "sm",
-                      })}>Sign In</Link>
-                )}
-                <ModeToggle />
-
-            </div>
+  const session = await getAuthSession();
+  return (
+    <div className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="container flex h-16 items-center">
+        <div className="hidden gap-6 lg:flex">
+          {/* logo */}
+          <Link href="/" className="hidden items-center space-x-2 lg:flex">
+            <Icons.logo className="h-6 w-6" />
+            <p className="hidden font-bold lg:inline-block">
+              Rate My Interview
+            </p>
+          </Link>
+          <Link href="/company" className="flex gap-2 items-center">
+            <p className="hidden dark:text-zinc-300 text-sm font-medium md:block">
+              Companies
+            </p>
+          </Link>
         </div>
-    );
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          <nav className="flex items-center space-x-2">
+            <div className="flex gap-2 items-center">
+              <Combobox />
+            </div>
+
+            {/* search bar */}
+
+            {session?.user ? (
+              <UserAccountNav user={session.user} />
+            ) : (
+              <Link
+                href="/sign-in"
+                className={buttonVariants({
+                  size: "sm",
+                })}
+              >
+                Sign In
+              </Link>
+            )}
+            <ThemeToggle />
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
